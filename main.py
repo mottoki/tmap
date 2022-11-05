56338448e8c71ae2f055ffe0c897b4fd0646bd54
shintarot <tokimoto.shintaro@gmail.com>
2022-11-05 09:20:56 +0800

    editable images

--- main.py
+++ main.py
@@ -100,28 +100,35 @@ selected = option_menu(menu_title=None,
 m = folium.Map(location=[lat, lon], zoom_start=initial_zoom) #tiles='CartoDB dark_matter'
 
 # Getting data from DETA and create dataframe
-df = pd.DataFrame(columns=['key', 'latitude', 'longitude', 'location', 'country', 'category', 'rating', 'period', 'comment', 'image'])
-all_cat = ["Food", "Entertainment", "Nature"]
-caticon = {all_cat[0]:'glass', all_cat[1]:'heart', all_cat[2]:'leaf'}
-catcol = {all_cat[0]:'coral', all_cat[1]:'darkturquoise', all_cat[2]:'palegreen'}
-for item in dataitems:
-    dkey = item['key']
-    dlat = item['latitude']
-    dlon = item['longitude']
-    dloc = item['locality']
-    dcon = item['country']
-    dcat = item['category']
-    drat = item['rating']
-    dper = item['period']
-    dcom = item['comment']
-    dima = item['image']
-    df.loc[len(df)] = [dkey, dlat, dlon, dloc, dcon, dcat, drat, dper, dcom, dima]
+cols = ['key', 'latitude', 'longitude', 'location', 'country', 'category', 'rating', 'period', 'comment', 'image']
+all_cat = ["Food", "Drink", "Shopping", "Activity", "Accomodation", "View Point"]
+caticon = {all_cat[0]:'leaf', all_cat[1]:'glass', all_cat[2]:'gift', all_cat[3]:'bicycle', all_cat[4]:'home', all_cat[5]:'camera'}
+catcol = {all_cat[0]:'coral', all_cat[1]:'darkturquoise', all_cat[2]:'palegreen', all_cat[3]:'#FEA3AA', all_cat[4]:'#9E7BFF', all_cat[5]:'#E9AB17'}
 
-    # Folium Marker
-    folium.Marker(location=[dlat, dlon], popup=dloc,
-        icon=BeautifyIcon(icon=caticon[dcat], icon_shape="marker",
-            border_color=catcol[dcat], background_color=catcol[dcat])).add_to(m)
-        # icon=folium.Icon(icon=caticon[dcat], color=catcol[dcat])).add_to(m)
+def retrieve_markers(dataitems, df, all_cat, caticon, catcol, draggable):
+    for item in dataitems:
+        dkey = item['key']
+        dlat = item['latitude']
+        dlon = item['longitude']
+        dloc = item['locality']
+        dcon = item['country']
+        dcat = item['category']
+        drat = item['rating']
+        dper = item['period']
+        dcom = item['comment']
+        dima = item['image']
+        df.loc[len(df)] = [dkey, dlat, dlon, dloc, dcon, dcat, drat, dper, dcom, dima]
+
+        # Folium Marker
+        folium.Marker(location=[dlat, dlon], popup=dloc, draggable=draggable,
+            icon=BeautifyIcon(icon=caticon[dcat], icon_shape="marker",
+                border_color=catcol[dcat], background_color=catcol[dcat])).add_to(m)
+            # icon=folium.Icon(icon=caticon[dcat], color=catcol[dcat])).add_to(m)
+        return df
+
+df = pd.DataFrame(columns=cols)
+draggable = False
+df = retrieve_markers(dataitems, df, all_cat, caticon, catcol, draggable)
 
 if selected == select_options[0]:
     col1, col2 = st.columns([1,1])
@@ -162,14 +169,14 @@ if selected == select_options[1]:
     # Add marker
     col1, col2 = st.columns([1,1])
     with col1:
+        # m = folium.Map(location=[lat, lon], draggable=True, zoom_start=initial_zoom)
+        df = pd.DataFrame(columns=cols)
+        draggable = True
+        df = retrieve_markers(dataitems, df, all_cat, caticon, catcol, draggable)
         Draw().add_to(m) # Draw(export=True)
         output = st_folium(m, width=350, height=450) #width=725
-        # try:
-        #     lobj_lat = output['last_object_clicked']['lat']
-        #     lobj_lon = output['last_object_clicked']['lng']
-        # except:
-        #     lobj_lat = None
-        #     lobj_lon = None
+        # print(output)
+
     with col2:
         try:
             last_obj_inp = output['last_object_clicked']
@@ -191,9 +198,23 @@ if selected == select_options[1]:
             loglocality = st.text_input("Location", disploc, key='loglocality')
             logcountry = st.text_input("Country", dispcon, key='logcountry')
             category = st.selectbox("Category", all_cat, index=cat_index, key='category')
-            rating = st.selectbox("Star", (1, 2, 3, 4, 5), index=disprat-1, key='rating')
+            rating = st.radio("Star", (1, 2, 3, 4, 5), index=disprat-1, horizontal=True, key='rating')
             mydate = st.date_input("Date", datetime.strptime(dispper, '%Y-%m-%d'), key='mydate')
             comment = st.text_area('Comments', dispcom, key='comment')
+
+            imgselect = [dispima[key].split('/')[-1] for key in dispima]
+            imgoptions = st.multiselect('Select your images', imgselect, imgselect)
+            imglinkdict = dict()
+            for imgname in imgoptions:
+                imglink = f'https://storage.googleapis.com/{bucket_name}/{imgname}'
+                imglinkdict[imgname] = imglink
+                st.markdown(imgname)
+                st.image(imglink, use_column_width='always')
+            image_files = st.file_uploader("Upload More Images", type=["png","jpg","jpeg"],
+                accept_multiple_files=True, key='image_files')
+            for image_file in image_files:
+                st.image(load_image(image_file), width=250)
+
         else:
             loglocality = st.text_input("Location", key='loglocality')
             logcountry = st.text_input("Country", country, key='logcountry')
@@ -208,48 +229,55 @@ if selected == select_options[1]:
             mydate = st.date_input("Date", datetime.today(), key='mydate')
             comment = st.text_area('Comments', key='comment')
 
-    if last_obj_inp:
-        for key in dispima:
-            st.image(dispima[key], use_column_width='always')
-    else:
-        image_files = st.file_uploader("Upload Images", type=["png","jpg","jpeg"],
-            accept_multiple_files=True, key='image_files')
-        # image_data = image_file.read()
-        # if image_file is not None:
-            # file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
-            # st.write(file_details)
-        for image_file in image_files:
-            st.image(load_image(image_file), width=250)
-
-    dictimages = {}
-    if st.button("Submit"):
-
-        if last_obj_inp:
-            loglocality = st.session_state['loglocality']
-            logcountry = st.session_state['logcountry']
-            category = st.session_state['category']
-            rating = st.session_state['rating']
-            mydate = str(st.session_state['mydate'])
-            comment = st.session_state['comment']
-            mykey = dispkey
-            dictimages = dispima
-        else:
-            loglocality = st.session_state['loglocality']
-            logcountry = st.session_state['logcountry']
-            category = st.session_state['category']
-            rating = st.session_state['rating']
-            mydate = str(st.session_state['mydate'])
-            comment = st.session_state['comment']
-            mykey = mydate+"_"+loglocality
-            image_files = [image_file for image_file in st.session_state['image_files']]
+            image_files = st.file_uploader("Upload Images", type=["png","jpg","jpeg"],
+                accept_multiple_files=True, key='image_files')
             for image_file in image_files:
-                bytedata = image_file.read()
-                image_url = upload_to_bucket(image_file.name, image_file.type,  bytedata, bucket_name)
-                dictimages[image_file.name]=image_url
+                st.image(load_image(image_file), width=250)
 
-        # print(image_url, image_file.name, image_file.type, bucket_name)
-        db.insert_location(mykey, loglocality, logcountry, loglat, loglon, category, rating, mydate, comment, dictimages)
-        st.success('Success!', icon="✅")
+    # if last_obj_inp:
+    #     for key in dispima:
+    #         st.image(dispima[key], use_column_width='always')
+    # else:
+    #     image_files = st.file_uploader("Upload Images", type=["png","jpg","jpeg"],
+    #         accept_multiple_files=True, key='image_files')
+    #     for image_file in image_files:
+    #         st.image(load_image(image_file), width=250)
+
+        dictimages = {}
+        if st.button("Submit"):
+
+            if last_obj_inp:
+                loglocality = st.session_state['loglocality']
+                logcountry = st.session_state['logcountry']
+                category = st.session_state['category']
+                rating = st.session_state['rating']
+                mydate = str(st.session_state['mydate'])
+                comment = st.session_state['comment']
+                mykey = dispkey
+                image_files = [image_file for image_file in st.session_state['image_files']]
+                for image_file in image_files:
+                    bytedata = image_file.read()
+                    image_url = upload_to_bucket(image_file.name, image_file.type,  bytedata, bucket_name)
+                    imglinkdict[image_file.name]=image_url
+                # print(imglinkdict)
+                dictimages = imglinkdict
+            else:
+                loglocality = st.session_state['loglocality']
+                logcountry = st.session_state['logcountry']
+                category = st.session_state['category']
+                rating = st.session_state['rating']
+                mydate = str(st.session_state['mydate'])
+                comment = st.session_state['comment']
+                mykey = mydate+"_"+loglocality
+                image_files = [image_file for image_file in st.session_state['image_files']]
+                for image_file in image_files:
+                    bytedata = image_file.read()
+                    image_url = upload_to_bucket(image_file.name, image_file.type,  bytedata, bucket_name)
+                    dictimages[image_file.name]=image_url
+
+            # print(image_url, image_file.name, image_file.type, bucket_name)
+            db.insert_location(mykey, loglocality, logcountry, loglat, loglon, category, rating, mydate, comment, dictimages)
+            st.success('Success!', icon="✅")
 
 
 
